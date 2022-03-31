63cad45749dc877181a81645b7b2307049c83c1a
SebastianNEON <sebastian@neon.dev>
2022-03-31 10:11:58 +0200

    update

--- lib/neon_downloader.dart
+++ lib/neon_downloader.dart
@@ -2,3 +2,5 @@ library neon_downloader;
 
 export 'src/utils/video_state.dart';
 export 'src/utils/video_downloader.dart';
+export 'src/domain/entitys/config_entity.dart';
+export 'src/domain/entitys/video_entity.dart';
